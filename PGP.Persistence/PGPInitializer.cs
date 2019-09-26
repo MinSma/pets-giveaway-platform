4639975fbb57c29ed363a63905edb8b942e76509
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-09-27 00:17:03 +0300

    Add database initializer

---
+++ PGP.Persistence/PGPInitializer.cs
@@ -0,0 +1,61 @@
+﻿using PGP.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Type = PGP.Domain.Entities.Type;
+
+namespace PGP.Persistence
+{
+    public class PGPInitializer
+    {
+        private readonly Dictionary<int, Role> Roles = new Dictionary<int, Role>();
+
+        public static void Initialize(PGPDbContext context)
+        {
+            var initializer = new PGPInitializer();
+            initializer.SeedEverything(context);
+        }
+
+        public void SeedEverything(PGPDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Roles.Any())
+            {
+                SeedRoles(context);
+            }
+
+            if (!context.Types.Any())
+            {
+                SeedTypes(context);
+            }
+        }
+
+        private void SeedRoles(PGPDbContext context)
+        {
+            var roles = new[]
+            {
+                new Role { Title = "User" },
+                new Role { Title = "Moderator" },
+                new Role { Title = "Admin" }
+            };
+
+            context.Roles.AddRange(roles);
+
+            context.SaveChanges();
+        }
+
+        private void SeedTypes(PGPDbContext context)
+        {
+            var types = new[]
+            {
+                new Type { Title = "Dogs" },
+                new Type { Title = "Cats" },
+                new Type { Title = "Others" }
+            };
+
+            context.Types.AddRange(types);
+
+            context.SaveChanges();
+        }
+    }
+}
--- PGP/Program.cs
+++ PGP/Program.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PGP.Persistence;
+using System;
+
 
 namespace PGP
 {
@@ -7,7 +13,26 @@ namespace PGP
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<IPGPDbContext>();
+
+                    var concreteContext = (PGPDbContext)context;
+                    concreteContext.Database.Migrate();
+                    PGPInitializer.Initialize(concreteContext);
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
