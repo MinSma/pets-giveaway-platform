2670ef1927cada29a2c7da0af20f4fdca120301f
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-10-19 20:53:38 +0300

    Add GetAllPets tests

---
+++ PGP.WebUI.IntegrationTests/Controllers/Pets/GetAllPetsTests.cs
@@ -0,0 +1,33 @@
+﻿using PGP.Application.Pets.Queries.GetAllPets;
+using PGP.WebUI.IntegrationTests.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PGP.WebUI.IntegrationTests.Controllers.Pets
+{
+    public class GetAllPetsTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public GetAllPetsTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnSuccessResult()
+        {
+            var client = await _factory.GetAnonymousClient();
+            
+            var response = await client.GetAsync("/api/pets");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await Utilities.GetResponseContent<List<GetAllPetsQueryResponse>>(response);
+
+            Assert.IsType<List<GetAllPetsQueryResponse>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
