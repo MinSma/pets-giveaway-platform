0a76c6b52d65683e63593fc7e4e87ce1543ae647
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-12-05 23:25:07 +0200

    Remove GetAllRolesQuery logic

--- PGP.Application/Roles/Queres/GetAllRoles/GetAllRolesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using System.Collections.Generic;
-
-namespace PGP.Application.Roles.Queries.GetRoles
-{
-    public class GetAllRolesQuery : IRequest<List<GetAllRolesQueryResponse>>
-    {
-    }
-}
--- PGP.Application/Roles/Queres/GetAllRoles/GetAllRolesQueryResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PGP.Application.Roles.Queries.GetRoles
-{
-    public class GetAllRolesQueryResponse
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-    }
-}
