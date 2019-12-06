640ce9f87aabcfcf25860d1af39a2ba14ca869e7
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-12-06 23:43:23 +0200

    Change font family to Titillium

--- PGP.WebUI/ClientApp/src/index.tsx
+++ PGP.WebUI/ClientApp/src/index.tsx
@@ -1,7 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './App';
 import './index.css';
+
+WebFont.load({
+    google: {
+      families: ['Titillium Web: 400', 'sans-serif']
+    }
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
