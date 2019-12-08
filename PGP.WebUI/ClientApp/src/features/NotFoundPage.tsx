6583eb9f92f84a2ab9e8739141d0bf4ae4ddf776
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-12-08 20:01:14 +0200

    Edit NotFoundPage text margins

--- PGP.WebUI/ClientApp/src/features/NotFoundPage.tsx
+++ PGP.WebUI/ClientApp/src/features/NotFoundPage.tsx
@@ -8,9 +8,11 @@ const NotFoundPage: React.FC = () => {
     return (
         <div className="container mt-5 mb-5 text-center">
             <h1>401</h1>
-            <h2>Page not found</h2>
-            <h4>Sorry, we couldn't find the page you were looking for. We suggest that you return to main sections.</h4>
-            <Button onClick={() => history.push('/')}>Go to the main page</Button>
+            <h2 className="mt-4">Page not found</h2>
+            <h4 className="mt-4">Sorry, we couldn't find the page you were looking for. We suggest that you return to main sections.</h4>
+            <Button className="mt-4" onClick={() => history.push('/')}>
+                Go to the main page
+            </Button>
         </div>
     );
 };
